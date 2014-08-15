2e9128239bb826fc7a60d2aa12e243890de16f56
Iain MacDonald <IJMacD@gmail.com>
2014-08-15 16:36:08 +0800

    Grade text based on simple word lists

---
+++ js/grader.js
@@ -0,0 +1,242 @@
+$(function(){
+		/*
+		 * DOM
+		 */
+	var sourceText = $('#source-text'),
+		resultText = $('#result'),
+		breakdownList = $('#breakdown'),
+		gradeButton = $('#grade-btn'),
+		sample1Button = $('#sample-1-btn'),
+		sample2Button = $('#sample-2-btn'),
+		sample3Button = $('#sample-3-btn'),
+
+		/*
+		 * Parameters
+		 */
+		wordLists = {
+			"Pre": ["see", "play", "me", "at", "run", "go", "and", "look", "can", "here"],
+			"Grade K": ["you", "come", "not", "with", "jump", "help", "is", "work", "are", "this"],
+			"Grade 1": ["road", "live", "thank", "when", "bigger", "how", "always", "night", "spring", "today"],
+			"Grade 2": ["our", "please", "myself", "town", "early", "send", "wide", "believe", "quietly", "carefully"],
+			"Grade 3": ["city", "middle", "moment", "frightened", "exclaimed", "several", "lonely", "drew", "since", "straight"],
+			"Grade 4": ["decided", "served", "amazed", "silent", "wrecked", "improved", "certainly", "entered", "realized", "interrupted"],
+			"Grade 5": ["scanty", "business", "develop", "considered", "discussed", "behaved", "splendid", "acquainted", "escaped", "grim"],
+			"Grade 6": ["bridge", "commercial", "abolish", "trucker", "apparatus", "elementary", "comment", "necessity", "gallery", "relativity"],
+			"Grade 7": ["amber", "dominion", "sundry", "capillary", "impetuous", "blight", "wrest", "enumerate", "daunted", "condescend"],
+			"Grade 8": ["capacious", "limitation", "pretext", "intrigue", "delusion", "immaculate", "ascent", "acrid", "binocular", "embankment"],
+			"Grade 9": ["conscientious", "isolation", "molecule", "ritual", "momentous", "vulnerable", "kinship", "conservation", "jaunty", "inventive"],
+			"Grade 10": ["zany", "jerkin", "nausea", "gratuitous", "linear", "inept", "legality", "aspen", "amnesty", "barometer"],
+			"Grade 11": ["galore", "rotunda", "capitalism", "prevaricate", "visible", "exonerate", "superannuate", "luxuriate", "piebald", "crunch"]
+		},
+
+		/*
+		 * Configuration
+		 */
+		sampleTexts = [
+			"The boy is playing with his dog. You can see it run towards this tree. Look now it is jumping! How does it always manage to scare me?",
+			"I am both amazed and frightened by your ability to make me feel lonely. It certainly makes me develop a feeling that I am daunted by. I considered there to be a kinship with you. Instead I feel like that was just a delusion. I feel such isolation as though an inept blight befalls me.",
+			"Oh what galore! Such a visible, momentous occasion. You have chosen to exonerate me! The acrid stench which once caused nausea, not from mere molecules, but from that truly zany situation."
+		],
+
+		/*
+		 * Data
+		 */
+		wordRegex = /\w+/g;
+
+	/*
+	 * Event Handlers
+	 */
+	gradeButton.on("click", gradeSourceText);
+	
+	sample1Button.on("click", function() {
+		sourceText.val(sampleTexts[0]);
+		
+		gradeSourceText();
+	});
+	
+	sample2Button.on("click", function() {
+		sourceText.val(sampleTexts[1]);
+		
+		gradeSourceText();
+	});
+	
+	sample3Button.on("click", function() {
+		sourceText.val(sampleTexts[2]);
+		
+		gradeSourceText();
+	});
+	
+
+	/*
+	 * Init
+	 */
+	sortWordLists();
+
+	/*
+	 * Functions
+	 */
+	function gradeSourceText(){
+		var text = sourceText.val(),
+			breakdown = gradeText(text);
+		
+		showGrade(breakdown);
+		showBreakdown(breakdown);
+	}
+	 
+	function gradeText(text){
+		var decomposed = decomposeText(text),
+			grades = {},
+			totalWords = getTotalWords(decomposed),
+			currentGrade,
+			grade,
+			wordList;
+		
+		for(grade in wordLists){
+			if(wordLists.hasOwnProperty(grade)){
+			
+				wordList = wordLists[grade];
+				
+				currentGrade = 0;
+				
+				wordList.forEach(function(word){
+					if(decomposed[word]){
+						currentGrade++;
+					}
+				});
+				
+				grades[grade] = currentGrade / totalWords;
+			}
+		}
+		
+		return grades;
+	}
+	 
+	function sortWordLists(){
+		for(grade in wordLists){
+			if(wordLists.hasOwnProperty(grade)){
+				wordLists[grade].sort();
+			}
+		}
+	}
+	
+	function decomposeText(text){
+		var words = text.match(wordRegex),
+			hash = {};
+		words.forEach(function(word){
+			word = word.toLowerCase();
+			if(hash[word]){
+				hash[word]++;
+			} else {
+				hash[word] = 1;
+			}
+		});
+		return hash;
+	}
+	
+	function getTotalWords(hash){
+		var word,
+			count = 0;
+		
+		for(word in hash){
+			if(hash.hasOwnProperty(word)){
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	function calculateGrade(grades){
+		return calculatePeakGrade(grades);
+	}
+	
+	function calculatePeakGrade(grades){
+		var max = 0,
+			grade,
+			value,
+			peaks = {};
+		
+		for(grade in grades){
+			if(grades.hasOwnProperty(grade)){
+				value = grades[grade];
+				
+				max = Math.max(max, value);
+			}
+		}
+
+		for(grade in grades){
+			if(grades.hasOwnProperty(grade)){
+				value = grades[grade];
+				
+				if(value == max){
+					peaks[grade] = value;
+				}
+			}
+		}
+		
+		return calculateAverageGrade(peaks);
+	}
+	
+	function calculateAverageGrade(grades){
+		var grade,
+			value,
+			i = 1,
+			sum = 0,
+			total = 0,
+			index = [];
+		
+		for(grade in grades){
+			if(grades.hasOwnProperty(grade)){
+				index.push(grade);
+			
+				value = grades[grade];
+				
+				sum += i * value;
+				total += value;
+				
+				i++;
+			}
+		}
+		
+		avg = sum / total;
+		
+		console.log(sum, total, avg);
+		
+		return index[Math.floor(avg-1)];
+	}
+	
+	function showGrade(grades){
+		var grade = calculateGrade(grades);
+		
+		resultText.text(grade);
+		resultText.show();
+	}
+	
+	function showBreakdown(grades){
+		var grade,
+			value,
+			r,
+			g,
+			b,
+			calcGrade = calculateGrade(grades);
+		
+		breakdownList.empty();
+		
+		for(grade in grades){
+			if(grades.hasOwnProperty(grade)){
+				value = grades[grade];
+				r = 255*(1-value);
+				g = 255*Math.sin(value*Math.PI*2);
+				b = 128;
+				$('<li title="'+grade+'">'+(value*100).toFixed()+'%</li>')
+					.css({
+						'height': value*200,
+						'background': 'rgba('+r.toFixed()+', '+g.toFixed()+', '+b.toFixed()+', 1)',
+						'box-shadow': (grade==calcGrade)?'0 0 5px 5px yellow':''
+					})
+					.appendTo(breakdownList);
+			}
+		}
+		
+		breakdownList.show();
+	}
+});
